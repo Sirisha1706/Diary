fc98081d745857bc609bbf888468c18a5d990de8
Sirisha1706 <sirishasundharisadhu@gmail.com>
2022-04-09 19:41:45 +0530

    first commit

--- src/Components/Home/Notes/Notesitem.js
+++ src/Components/Home/Notes/Notesitem.js
@@ -4,12 +4,6 @@ import Notedate from './Notedate';
 import './Notesitem.css';
 
 const Notesitem = props =>{
-    const [list , setlist] = useState([props]);
-    
-    const removeHandler =(id) =>{
-        const newlist = list.filter((item) =>item.id !=id);
-        setlist(newlist);
-    };
     return (
         <li key={props.id}>
         <Card className='notes-item'>
@@ -18,7 +12,7 @@ const Notesitem = props =>{
             <h1>{props.header}</h1>
             <p>{props.notes}</p>
             </div>
-            <button onClick={()=>removeHandler(props.id)}>DELETE</button>
+           
         </Card>
         </li>
     );
